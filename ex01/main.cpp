093f5f7332228378461d20e54d1d0ebece5848a1
7Steffen7 <steffenparth@mbp-von-steffen.speedport.ip>
2024-09-28 00:12:19 +0200

    ex01: brainstorming and preperation

---
+++ ex01/main.cpp
@@ -0,0 +1,22 @@
+#include "RPN.hpp"
+
+/*
+parsing:
+- seperate by space
+- max amount of numbers == 10
+- if string numberic convert to int - std::stoi
+- always have to start with 2 numbers than operator, num, operator, num - everything else invalid
+- stacksize should always be between 1 or 2
+- when enconter an opertor (stacksize must be 2) pop one out and do operation
+
+
+
+
+
+*/
+
+
+int main(int argc, char* argv[]) {
+
+	return 0;
+}
